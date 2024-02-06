d3a3b148673c97c888ada7c8ae2790f8afc31b8b
FCarneiroOliveira <107850122+FCarneiroOliveira@users.noreply.github.com>
2024-02-06 17:37:45 -0400

    aula 21 e treino calculadora

---
+++ Udemy/aula21/operternario.js
@@ -0,0 +1,9 @@
+// ? :
+const pontUsuario = 1999;
+const nivUsuario = pontUsuario >= 1000 ? 'Usuário VIP!' : 'Usuário normal';
+console.log(nivUsuario)
+// if(pontUsuario >= 1000){
+//     console.log('Usuário VIP!')
+// }else{
+//     console.log('Usuário normal')
+// }
